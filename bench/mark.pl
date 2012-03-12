99b3455bbd0469aa876ab1a784127a4a75f7c21b
Pavel Shaydo <zwon@cpan.org>
2012-03-12 22:14:25 +0800

    update bench/mark.pl

--- bench/mark.pl
+++ bench/mark.pl
@@ -7,17 +7,23 @@ use RedisDB;
 use Redis::hiredis;
 use lib qw(t ../t);
 use RedisServer;
+use Redis::Client;
 
 use Benchmark qw( cmpthese );
+
+# benchmark some redis clients.  Note, that I'm getting quite a different
+# results in different environments, need to review this thing.
 
 say "Testing against";
 say "RedisDB:        ", RedisDB->VERSION;
 say "Redis:          ", Redis->VERSION;
 say "Redis::hiredis: ", Redis::hiredis->VERSION;
+say "Redis::Client:  ", Redis::Client->VERSION;
 
 my $srv     = RedisServer->start;
-my $redis   = Redis->new( server => "localhost:$srv->{port}" );
+my $redis   = Redis->new( server => "localhost:$srv->{port}", encoding => undef, );
 my $redisdb = RedisDB->new( host => "localhost", port => $srv->{port} );
+my $rediscl = Redis::Client->new( host => "localhost", port => $srv->{port} );
 my $hiredis = Redis::hiredis->new();
 $hiredis->connect('localhost');
 
@@ -29,61 +35,70 @@ sub sender {
     }
 }
 
-cmpthese - 5, {
+say '';
+
+cmpthese 150, {
     Redis => sub {
-        sender( $redis, 10000, "0123456789abcdef" );
+        sender( $redis, 1000, "0123456789abcdef" );
     },
     hiredis => sub {
-        sender( $hiredis, 10000, "0123456789abcdef" );
+        sender( $hiredis, 1000, "0123456789abcdef" );
     },
     RedisDB => sub {
-        sender( $redisdb, 10000, "0123456789abcdef" );
+        sender( $redisdb, 1000, "0123456789abcdef" );
+    },
+    "Redis::Client" => sub {
+        sender( $rediscl, 1000, "0123456789abcdef" );
     },
     "RedisDB Pipelining" => sub {
-        for ( 1 .. 10000 ) {
-            $redisdb->send_command( 'SET', "RDB$_", "0123456789abcdef" );
-            $redisdb->send_command( 'GET', "RDB$_" );
+        for ( 1 .. 1000 ) {
+            $redisdb->set( "RDB$_", "0123456789abcdef", RedisDB::IGNORE_REPLY );
+            $redisdb->get( "RDB$_", RedisDB::IGNORE_REPLY );
         }
-        my %res;
-        for ( 1 .. 20000 ) {
-            $res{ $redisdb->get_reply }++;
-        }
-        die "wrong result" unless $res{'0123456789abcdef'} == 10000;
+        $redisdb->mainloop;
     },
     "hiredis pipelining" => sub {
-        for ( 1 .. 10000 ) {
+        for ( 1 .. 1000 ) {
             $hiredis->append_command("SET RDB$_ 0123456789abcdef");
             $hiredis->append_command("GET RDB$_");
         }
         my %res;
-        for ( 1 .. 20000 ) {
+        for ( 1 .. 2000 ) {
             $res{ $hiredis->get_reply }++;
         }
-        die "wrong result" unless $res{'0123456789abcdef'} == 10000;
-        die "wrong result" unless $res{'OK'} == 10000;
-      }
-};
-
-cmpthese - 5, {
-    Redis => sub {
-        sender( $redis, 3000, "0123456789abcdef" x 128 );
-    },
-    hiredis => sub {
-        sender( $hiredis, 3000, "0123456789abcdef" x 128 );
-    },
-    RedisDB => sub {
-        sender( $redisdb, 3000, "0123456789abcdef" x 128 );
     },
 };
 
-cmpthese - 5, {
+say '';
+
+cmpthese 150, {
     Redis => sub {
-        sender( $redis, 1000, "0123456789abcdef" x 1024 );
+        sender( $redis, 300, "0123456789abcdef" x 128 );
     },
     hiredis => sub {
-        sender( $hiredis, 1000, "0123456789abcdef" x 128 );
+        sender( $hiredis, 300, "0123456789abcdef" x 128 );
     },
     RedisDB => sub {
-        sender( $redisdb, 1000, "0123456789abcdef" x 1024 );
+        sender( $redisdb, 300, "0123456789abcdef" x 128 );
+    },
+    "Redis::Client" => sub {
+        sender( $rediscl, 300, "0123456789abcdef" x 128 );
+    },
+};
+
+say '';
+
+cmpthese 150, {
+    Redis => sub {
+        sender( $redis, 100, "0123456789abcdef" x 1024 );
+    },
+    hiredis => sub {
+        sender( $hiredis, 100, "0123456789abcdef" x 1024 );
+    },
+    RedisDB => sub {
+        sender( $redisdb, 100, "0123456789abcdef" x 1024 );
+    },
+    "Redis::Client" => sub {
+        sender( $rediscl, 100, "0123456789abcdef" x 1024 );
     },
 };
