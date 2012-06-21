984654daf0a5e392ce7a2bd1bccdc1471a2b369f
Pavel Shaydo <zwon@cpan.org>
2012-06-21 23:27:03 +0800

    bump version 1.99_04

--- lib/RedisDB/Error.pm
+++ lib/RedisDB/Error.pm
@@ -2,7 +2,7 @@ package RedisDB::Error;
 
 use strict;
 use warnings;
-our $VERSION = "1.99_03";
+our $VERSION = "1.99_04";
 $VERSION = eval $VERSION;
 
 =head1 NAME
--- lib/RedisDB/Parse/Redis_XS.pm
+++ lib/RedisDB/Parse/Redis_XS.pm
@@ -1,7 +1,7 @@
 package RedisDB::Parse::Redis_XS;
 use strict;
 use warnings;
-our $VERSION = "1.99_03";
+our $VERSION = "1.99_04";
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
