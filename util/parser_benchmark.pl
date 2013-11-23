5c113586ff40e392cc78bee0fea6808461082572
Pavel Shaydo <zwon@cpan.org>
2013-11-23 20:40:47 +0800

    add Parse::Redis::XS into benchmark

--- util/parser_benchmark.pl
+++ util/parser_benchmark.pl
@@ -7,9 +7,12 @@ use RedisDB::Parse::Redis_XS;
 use RedisDB::Parse::Redis_PP;
 use Protocol::Redis;
 use Protocol::Redis::XS;
+use Redis::Parser::XS;
 use Data::Dumper;
 
 $Data::Dumper::Sortkeys = 1;
+
+say "Please read source code to make sure you understand what this script is benchmarking";
 
 sub eq_or_die {
     my ( $got, $expected, $parser ) = @_;
@@ -26,10 +29,11 @@ my $reply = join "\r\n", qw(
   hhhhhhhhh
   ), "";
 
+my $true = sub { 1 };
 my $rdb_pp = RedisDB::Parse::Redis_PP->new;
-$rdb_pp->set_default_callback( sub { 1 } );
+$rdb_pp->set_default_callback($true);
 my $rdb_xs = RedisDB::Parse::Redis_XS->new;
-$rdb_xs->set_default_callback( sub { 1 } );
+$rdb_xs->set_default_callback($true);
 
 my $rdb_parsed = [ 123, undef, 'hhhhhhhhh' ];
 $rdb_pp->add_callback( sub { eq_or_die( $_[1], $rdb_parsed, "RedisDB PP" ) } );
@@ -59,17 +63,43 @@ my $pr_parsed = {
 };
 $pr_pp->on_message( sub { eq_or_die( $_[1], $pr_parsed, "Protocol::Redis" ) } );
 $pr_pp->parse($reply);
+
 # Protocol::Redis::XS returns slightly different result
 $pr_parsed->{data}[0]{data} = 123;
 $pr_xs->on_message( sub { eq_or_die( $_[1], $pr_parsed, "Protocol::Redis::XS" ) } );
 $pr_xs->parse($reply);
-$pr_pp->on_message( sub { 1 } );
-$pr_xs->on_message( sub { 1 } );
+$pr_pp->on_message($true);
+$pr_xs->on_message($true);
 
-cmpthese - 5,
-  {
-    "RedisDB PP"          => sub { $rdb_pp->add($reply)  for 1 .. 1000 },
-    "RedisDB XS"          => sub { $rdb_xs->add($reply)  for 1 .. 1000 },
-    "Protocol::Redis"     => sub { $pr_pp->parse($reply) for 1 .. 1000 },
-    "Protocol::Redis::XS" => sub { $pr_xs->parse($reply) for 1 .. 1000 },
-  };
+my $parse_redis_parsed = [ '*', [ [ ':', '123' ], undef, 'hhhhhhhhh' ] ];
+parse_redis $reply, \my @res;
+eq_or_die $res[0], $parse_redis_parsed, "Parse::Redis::XS";
+
+# comparing other parsers against bare parse_redis is not correct
+sub redis_parser {
+    state $buf = '';
+    $buf .= shift;
+    my $len = parse_redis $buf, \my @res;
+    substr $buf, 0, $len, '';
+    $true->($_) for @res;
+}
+
+my @replies = (
+    [ "Status message",  "+OK\r\n" ],
+    [ "Bulk reply",      "\$3\r\nfoo\r\n" ],
+    [ "Multibulk reply", $reply ]
+);
+
+for (@replies) {
+    my ( $test, $reply ) = @$_;
+    say "\nParsing $test\n";
+    cmpthese - 5,
+      {
+        "RedisDB PP"               => sub { $rdb_pp->add($reply)  for 1 .. 10000 },
+        "RedisDB XS"               => sub { $rdb_xs->add($reply)  for 1 .. 10000 },
+        "Protocol::Redis"          => sub { $pr_pp->parse($reply) for 1 .. 10000 },
+        "Protocol::Redis::XS"      => sub { $pr_xs->parse($reply) for 1 .. 10000 },
+        "wrapped Parse::Redis::XS" => sub { redis_parser($reply)  for 1 .. 10000 },
+        "bare Parse::Redis::XS" => sub { parse_redis $reply, \my @res for 1 .. 10000 },
+      };
+}
