8c5f4d5d2fe26e2ba719dcfadb192a67d941c662
Pavel Shaydo <zwon@cpan.org>
2013-12-29 22:17:57 +0800

    bump version 2.23

--- lib/RedisDB.pm
+++ lib/RedisDB.pm
@@ -2,7 +2,7 @@ package RedisDB;
 
 use strict;
 use warnings;
-our $VERSION = "2.22";
+our $VERSION = "2.23";
 $VERSION = eval $VERSION;
 
 use RedisDB::Error;
--- lib/RedisDB/Error.pm
+++ lib/RedisDB/Error.pm
@@ -2,7 +2,7 @@ package RedisDB::Error;
 
 use strict;
 use warnings;
-our $VERSION = "2.22";
+our $VERSION = "2.23";
 $VERSION = eval $VERSION;
 
 =head1 NAME
