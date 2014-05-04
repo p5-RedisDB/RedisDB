1d161ba7a7519b4a522c917c9683c88b853485b2
Pavel Shaydo <zwon@cpan.org>
2014-05-04 23:53:46 +0800

    document that there's no reconnect between MULTI and EXEC/DISCARD

--- lib/RedisDB.pm
+++ lib/RedisDB.pm
@@ -1085,7 +1085,10 @@ L<RedisDB::Error::DISCONNECTED> class, next time you use the object it will
 establish a new connection. If L</raise_error> disabled, the module will pass
 L<RedisDB::Error::DISCONNECTED> object to all outstanding callbacks and will
 try to reconnect to the server; it will also automatically restore
-subscriptions if object was in subscription mode.
+subscriptions if object was in subscription mode. Module never tries to
+reconnect after MULTI command was sent to server and before corresponding EXEC
+or DISCARD was sent as this may cause data corruption, so during transaction
+module behaves like if L</raise_error> is set.
 
 Module makes several attempts to reconnect each time increasing interval before
 the next attempt, depending on the values of L</reconnect_attempts> and
@@ -1443,10 +1446,10 @@ order to start a transaction you should use the I<multi> method.  After you
 have entered a transaction all the commands you issue are queued, but not
 executed till you call the I<exec> method. Typically these commands return
 string "QUEUED" as a result, but if there is an error in e.g. number of
-arguments, they may croak. When you call exec, all the queued commands will be
-executed and exec will return a list of results for every command in the
-transaction. If instead of I<exec> you call I<discard>, all scheduled commands
-will be canceled.
+arguments, they may return an error. When you call exec, all the queued
+commands will be executed and exec will return a list of results for every
+command in the transaction. If instead of I<exec> you call I<discard>, all
+scheduled commands will be canceled.
 
 You can set some keys as watched. If any watched key has been changed by
 another client before you called exec, the transaction will be discarded and
