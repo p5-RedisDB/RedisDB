f576d8e96f534e50ab309ae78a4c8990d52f0c5f
Pavel Shaydo <zwon@cpan.org>
2015-02-05 23:34:48 +0100

    add skeleton for RedisDB::Cluster. See #22

---
+++ lib/RedisDB/Cluster.pm
@@ -0,0 +1,40 @@
+package RedisDB::Cluster;
+
+use strict;
+use warnings;
+our $VERSION = "2.36";
+$VERSION = eval $VERSION;
+
+use RedisDB;
+
+=head1 NAME
+
+RedisDB::Cluster - client for redis cluster
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Pavel Shaydo, C<< <zwon at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011-2014 Pavel Shaydo.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
